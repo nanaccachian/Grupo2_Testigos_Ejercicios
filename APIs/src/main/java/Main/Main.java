6f840193693cd70d36b239f10b0a7686bcb7c8bd
Nicolas Naccachian <37561335+nanaccachian@users.noreply.github.com>
2020-09-08 20:17:15 -0300

    FIN APIs

--- APIs/src/main/java/Main/Main.java
+++ APIs/src/main/java/Main/Main.java
@@ -14,51 +14,68 @@ import Servicios.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        MLMain();
+        ML();
     }
 
-    public static void MLMain() throws IOException {
+    public static void ML() throws IOException {
         MercadoLibre.ServicioML servicioML = MercadoLibre.ServicioML.instancia();
+
         List<MunicipioML> municipiosML = new ArrayList<>();
 
         System.out.println("Seleccione una de las siguientes provincias, ingresando su id:");
 
+        //MUESTRA IDS PROVINCIAS PARA ELEGIR
         ListadoDeProvinciasML listadoDeProvinciasArgentinas = servicioML.listadoDeProvinciasML();
         for (ProvinciasML unaProvincia:listadoDeProvinciasArgentinas.states) {
             System.out.println(unaProvincia.getId() + ") " + unaProvincia.getName());
         }
 
+        //SCANEA LO QUE ELIGIO
         Scanner entradaEscaner = new Scanner(System.in);
         String idProvinciaElegida = entradaEscaner.nextLine();
 
+        //OBTIENE LA PROVINCIA DEL LISTADO
         Optional<ProvinciasML> posibleProvincia = listadoDeProvinciasArgentinas.provinciaDeId(idProvinciaElegida);
 
         if (posibleProvincia.isPresent()) {
+
             ProvinciasML provinciaSeleccionada = posibleProvincia.get();
             ListadoDeMunicipiosML municipiosDeLaProvincia = servicioML.listadoDeMunicipiosDeProvinciaML(provinciaSeleccionada);
+
+            //CONVIERTE EL LISTADO A UNA LISTA DE MUNICIPIOS
             for(MunicipioML unMunicipio: municipiosDeLaProvincia.cities)
                 municipiosML.add(new MunicipioML(unMunicipio.getId(),unMunicipio.getName()));
-            List<Municipio> municipiosGR = georefMain(posibleProvincia.get());
+
+            //PIDE LA LISTA DE MUNICIPIOS DE LA MISMA PROVINCIA PERO EN GEOREF
+            List<Municipio> municipiosGR = georef(posibleProvincia.get());
+
+            //COMPARA APIs
             compararAPIs(municipiosGR,municipiosML);
         } else System.out.println("No existe la provincia seleccionada");
 
     }
 
-    public static List<Municipio> georefMain(ProvinciasML prov) throws IOException {
+    public static List<Municipio> georef(ProvinciasML prov) throws IOException {
         List<Municipio> municipiosGR = new ArrayList<>();
         GeoRef.ServicioGeoref servicioGeoref = GeoRef.ServicioGeoref.instancia();
 
+        //OBTIENE LA LISTA DE MUNICIPIOS DE LA PROVINCIA
         ListadoDeMunicipios municipiosDeLaProvincia = servicioGeoref.listadoDeMunicipiosDeProvinciaConNombre(prov.getName());
 
+        //CONVIERTE EL LISTADO A UNA LISTA DE MUNICIPIOS
         for (Municipio unMunicipio : municipiosDeLaProvincia.municipios)
             municipiosGR.add(new Municipio(unMunicipio.getId(), unMunicipio.getNombre()));
+
         return municipiosGR;
     }
 
     private static void compararAPIs(List<Municipio> municipiosGR, List<MunicipioML> municipiosML) {
+
+        //SE FILTRAN AMBAS LISTAS
         List<Municipio> municipiosGR2 = municipiosGR.stream().filter(mun-> !mun.estaEnLista(municipiosML)).collect(Collectors.toList());
         List<MunicipioML> municpiosML2= municipiosML.stream().filter(mun-> !mun.estaEnLista(municipiosGR)).collect(Collectors.toList());
 
+        //SE MUESTRAN LAS DIFERENCIAS
         System.out.println("Los municipios que si tiene Georef y no tiene MercadoLibre son: ");
         mostrarLista(municipiosGR2);
         System.out.println("Los municipios que no tiene Georef y si tiene MercadoLibre son: ");
